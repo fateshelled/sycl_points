616651d42d15204bf3f3cd8ded76fdfe33014fb1
fateshelled <53618876+fateshelled@users.noreply.github.com>
2025-04-19 10:42:20 +0900

    update bitonic_sort

--- include/sycl_points/algorithms/core/bitonic_sort.hpp
+++ include/sycl_points/algorithms/core/bitonic_sort.hpp
@@ -10,92 +10,29 @@ namespace algorithms {
 
 namespace core {
 
-template <typename T>
-class BitonicSortDevice {
-public:
-    BitonicSortDevice(const std::shared_ptr<sycl::queue> &queue_ptr) : queue_ptr_(queue_ptr) {
-        this->padded_data_ = std::make_shared<ContainerDevice<T>>(this->queue_ptr_);
-        this->invalid_value_ = T();
-    }
-    void setInvalidValue(T value) { this->invalid_value_ = value; }
-
-    template <typename Allocator = std::allocator<T>>
-    void sort(const std::vector<T, Allocator> &input, std::vector<T, Allocator> &result) {
-        const auto N = input.size();
-
-        size_t pow2_size = 1;
-        while (pow2_size < N) {
-            pow2_size *= 2;
-        }
-
-        if (this->padded_data_->size < pow2_size) {
-            this->padded_data_->resize(pow2_size);
-        }
-        sycl_utils::events events;
-        events += this->padded_data_->memset_async(this->invalid_value_);
-        events += this->padded_data_->memcpy_async(input.data(), input.size());
-        events.wait();
-
-        // work group size
-        const size_t local_size = sycl_utils::get_work_group_size(*this->queue_ptr_);
-
-        // sort
-        for (size_t k = 2; k <= pow2_size; k *= 2) {
-            for (size_t j = k / 2; j > 0; j /= 2) {
-                auto event = this->queue_ptr_->submit([&](sycl::handler &h) {
-                    const auto data_ptr = this->padded_data_->data;
-                    //  h.depends_on(events.evs);  // too slow
-                    h.parallel_for(sycl::nd_range<1>(sycl::range<1>(pow2_size / 2), sycl::range<1>(local_size)),
-                                   [=](sycl::nd_item<1> item) {
-                                       const auto gid = item.get_global_id(0);
-
-                                       // if (gid >= pow2_size / 2) return;
-
-                                       const size_t i = 2 * gid - (gid & (j - 1));
-                                       const size_t ixj = i ^ j;
-
-                                       if (ixj > i && i < pow2_size && ixj < pow2_size) {
-                                           const bool direction = ((i & k) == 0);
-
-                                           if ((data_ptr[i] > data_ptr[ixj]) == direction) {
-                                               // swap
-                                               const T temp = data_ptr[i];
-                                               data_ptr[i] = data_ptr[ixj];
-                                               data_ptr[ixj] = temp;
-                                           }
-                                       }
-                                   });
-                });
-                //  event.wait();
-                events += event;
-            }
-        }
-        events.wait();
-
-        result.resize(N);
-        this->queue_ptr_->memcpy(result.data(), this->padded_data_->data, N * sizeof(T)).wait();
-    }
-
-private:
-    std::shared_ptr<sycl::queue> queue_ptr_ = nullptr;
-    std::shared_ptr<ContainerDevice<T>> padded_data_ = nullptr;
-    T invalid_value_;
-};
-
-template <typename T>
+template <typename T, size_t Alignment = 0>
 class BitonicSortShared {
 public:
     BitonicSortShared(const std::shared_ptr<sycl::queue> &queue_ptr) : queue_ptr_(queue_ptr) {
-        constexpr size_t INITIAL_SIZE = 65536;
-        this->padded_data_ = std::make_shared<shared_vector<T>>(INITIAL_SIZE, std::numeric_limits<T>::max(),
-                                                                shared_allocator<T>(*this->queue_ptr_, {}));
-        this->invalid_value_ = T();
+        constexpr size_t INITIAL_SIZE = 32768;
+        this->padded_data_ = std::make_shared<shared_vector<T, Alignment>>(
+            INITIAL_SIZE, shared_allocator<T, Alignment>(*this->queue_ptr_, {}));
+        this->padded_data_indices_ =
+            std::make_shared<shared_vector<uint64_t>>(INITIAL_SIZE, shared_allocator<uint64_t>(*this->queue_ptr_));
     }
-    void setInvalidValue(T value) { this->invalid_value_ = value; }
+
+    std::vector<uint64_t> get_sorted_indices() {
+        std::vector<uint64_t> indices(this->last_N_);
+        this->queue_ptr_->memcpy(indices.data(), this->padded_data_indices_->data(), this->last_N_ * sizeof(uint64_t))
+            .wait();
+        return indices;
+    }
 
     template <typename Allocator = std::allocator<T>>
-    void sort(const std::vector<T, Allocator> &input, std::vector<T, Allocator> &result) {
+    void sort(std::vector<T, Allocator> &input) {
         const auto N = input.size();
+
+        this->last_N_ = N;
 
         size_t pow2_size = 1;
         while (pow2_size < N) {
@@ -103,55 +40,73 @@ public:
         }
         if (this->padded_data_->size() < pow2_size) {
             this->padded_data_->resize(pow2_size);
+            this->padded_data_indices_->resize(pow2_size);
         }
+
         sycl_utils::events events;
-        events += this->queue_ptr_->memset(this->padded_data_->data() + N, this->invalid_value_, pow2_size - N);
+
+        std::vector<uint64_t> series(pow2_size);
+        std::iota(series.begin(), series.end(), 0);
+        events +=
+            this->queue_ptr_->memcpy(this->padded_data_indices_->data(), series.data(), pow2_size * sizeof(uint64_t));
+
+        events += this->queue_ptr_->fill(this->padded_data_->data() + N, std::numeric_limits<T>::max(), pow2_size - N);
         events += this->queue_ptr_->memcpy(this->padded_data_->data(), input.data(), N * sizeof(T));
         events.wait();
 
         // work group size
-        const size_t local_size = sycl_utils::get_work_group_size(*this->queue_ptr_);
+        const size_t max_local_size = sycl_utils::get_work_group_size(*this->queue_ptr_);
 
+        const auto data_ptr = this->padded_data_->data();
+        const auto indices_ptr = this->padded_data_indices_->data();
         // sort
         for (size_t k = 2; k <= pow2_size; k *= 2) {
             for (size_t j = k / 2; j > 0; j /= 2) {
+                const size_t local_size = std::min(max_local_size, pow2_size / 2);
+
                 auto event = this->queue_ptr_->submit([&](sycl::handler &h) {
-                    const auto data_ptr = this->padded_data_->data();
-                    //  h.depends_on(events.evs);  // too slow
                     h.parallel_for(sycl::nd_range<1>(sycl::range<1>(pow2_size / 2), sycl::range<1>(local_size)),
                                    [=](sycl::nd_item<1> item) {
                                        const auto gid = item.get_global_id(0);
 
-                                       // if (gid >= pow2_size / 2) return;
+                                       if (gid >= pow2_size / 2) return;
 
-                                       const size_t i = 2 * gid - (gid & (j - 1));
-                                       const size_t ixj = i ^ j;
+                                       const uint64_t i = 2 * gid - (gid & (j - 1));
+                                       const uint64_t ixj = i ^ j;
 
                                        if (ixj > i && i < pow2_size && ixj < pow2_size) {
-                                           const bool direction = ((i & k) == 0);
+                                              // necessary swap or not
+                                              const auto data_i = data_ptr[i];
+                                              const auto data_ixj = data_ptr[ixj];
+                                              const bool is_equal = (data_i == data_ixj);
+                                              const auto index_i = indices_ptr[i];
+                                              const auto index_ixj = indices_ptr[ixj];
+                                              const bool idx_compare = (index_i > index_ixj);
+                                              const bool val_compare = (data_i > data_ixj);
 
-                                           if ((data_ptr[i] > data_ptr[ixj]) == direction) {
-                                               // swap
-                                               const T temp = data_ptr[i];
-                                               data_ptr[i] = data_ptr[ixj];
-                                               data_ptr[ixj] = temp;
-                                           }
+                                              const bool should_swap = is_equal ? (idx_compare == ((i & k) == 0))
+                                                                                : (val_compare == ((i & k) == 0));
+                                              if (should_swap) {
+                                                  data_ptr[i] = data_ixj;
+                                                  data_ptr[ixj] = data_i;
+                                                  indices_ptr[i] = index_ixj;
+                                                  indices_ptr[ixj] = index_i;
+                                              }
                                        }
                                    });
                 });
-                events += event;
+                event.wait();
             }
         }
-        events.wait();
 
-        result.resize(N);
-        this->queue_ptr_->memcpy(result.data(), this->padded_data_->data(), N * sizeof(T)).wait();
+        this->queue_ptr_->memcpy(input.data(), this->padded_data_->data(), N * sizeof(T)).wait();
     }
 
 private:
     std::shared_ptr<sycl::queue> queue_ptr_ = nullptr;
-    std::shared_ptr<shared_vector<T>> padded_data_ = nullptr;
-    T invalid_value_;
+    std::shared_ptr<shared_vector<T, Alignment>> padded_data_ = nullptr;
+    std::shared_ptr<shared_vector<uint64_t>> padded_data_indices_ = nullptr;
+    size_t last_N_ = 0;
 };
 
 }  // namespace core
