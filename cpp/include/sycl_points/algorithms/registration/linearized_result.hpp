486be3221769fd76131117f1bf135ce012b387d9
fateshelled <53618876+fateshelled@users.noreply.github.com>
2025-12-20 00:47:17 +0900

    refactor: reorganize linearized result structure and update includes in factor.hpp

---
+++ cpp/include/sycl_points/algorithms/registration/linearized_result.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <limits>
+
+namespace sycl_points {
+
+namespace algorithms {
+
+namespace registration {
+/// @brief Registration Linearized Result
+struct LinearizedResult {
+    /// @brief Hessian, Information Matrix
+    Eigen::Matrix<float, 6, 6> H = Eigen::Matrix<float, 6, 6>::Zero();
+    /// @brief Gradient, Information Vector
+    Eigen::Vector<float, 6> b = Eigen::Vector<float, 6>::Zero();
+    /// @brief Error value
+    float error = std::numeric_limits<float>::max();
+    /// @brief inlier point num
+    uint32_t inlier = 0;
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+}  // namespace registration
+}  // namespace algorithms
+}  // namespace sycl_points
