31e192e96c5a45dd58a412f262db3702208d329e
fateshelled <53618876+fateshelled@users.noreply.github.com>
2026-01-14 19:11:18 +0900

    feat: use registration input point cloud for covariance markers

--- ros2/sycl_points_ros2/src/lidar_odometry_node.cpp
+++ ros2/sycl_points_ros2/src/lidar_odometry_node.cpp
@@ -132,7 +132,7 @@ void LiDAROdometryNode::point_cloud_callback(const sensor_msgs::msg::PointCloud2
             }
 
             this->covariance_marker_publisher_->publish_if_subscribed(msg->header,
-                                                                      this->pipeline_->get_preprocessed_point_cloud());
+                                                                      this->pipeline_->get_registration_input_point_cloud());
 
             if (this->pub_submap_->get_subscription_count() > 0) {
                 auto submap_msg = toROS2msg(this->pipeline_->get_submap_point_cloud(), msg->header);
